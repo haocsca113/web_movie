7fb0c7dc74567342ed74cb0402b432249b281431
mattHao <haocsca113@gmail.com>
2024-04-19 21:31:31 +0700

    19/04/2024

--- app/Http/Controllers/LoginFBController.php
+++ app/Http/Controllers/LoginFBController.php
@@ -1,126 +1,126 @@
 <?php
 
-namespace App\Http\Controllers;
+// namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use Laravel\Socialite\Facades\Socialite;
-use Exception;
-use App\Models\User;
-use Illuminate\Support\Facades\Auth;
+// use Illuminate\Http\Request;
+// use Laravel\Socialite\Facades\Socialite;
+// use Exception;
+// use App\Models\User;
+// use Illuminate\Support\Facades\Auth;
 
-class LoginFBController extends Controller
-{
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
+// class LoginFBController extends Controller
+// {
+//     /**
+//      * Display a listing of the resource.
+//      *
+//      * @return \Illuminate\Http\Response
+//      */
+//     public function index()
+//     {
+//         //
+//     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
+//     /**
+//      * Show the form for creating a new resource.
+//      *
+//      * @return \Illuminate\Http\Response
+//      */
+//     public function create()
+//     {
+//         //
+//     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
+//     /**
+//      * Store a newly created resource in storage.
+//      *
+//      * @param  \Illuminate\Http\Request  $request
+//      * @return \Illuminate\Http\Response
+//      */
+//     public function store(Request $request)
+//     {
+//         //
+//     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
+//     /**
+//      * Display the specified resource.
+//      *
+//      * @param  int  $id
+//      * @return \Illuminate\Http\Response
+//      */
+//     public function show($id)
+//     {
+//         //
+//     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
+//     /**
+//      * Show the form for editing the specified resource.
+//      *
+//      * @param  int  $id
+//      * @return \Illuminate\Http\Response
+//      */
+//     public function edit($id)
+//     {
+//         //
+//     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
+//     /**
+//      * Update the specified resource in storage.
+//      *
+//      * @param  \Illuminate\Http\Request  $request
+//      * @param  int  $id
+//      * @return \Illuminate\Http\Response
+//      */
+//     public function update(Request $request, $id)
+//     {
+//         //
+//     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
+//     /**
+//      * Remove the specified resource from storage.
+//      *
+//      * @param  int  $id
+//      * @return \Illuminate\Http\Response
+//      */
+//     public function destroy($id)
+//     {
+//         //
+//     }
 
 
-    public function redirectToFacebook()
-    {
-        return Socialite::driver('facebook')->redirect();
-    }
+//     public function redirectToFacebook()
+//     {
+//         return Socialite::driver('facebook')->redirect();
+//     }
 
-    public function handleFacebookCallback()
-    {
-        try {
+//     public function handleFacebookCallback()
+//     {
+//         try {
         
-            $user = Socialite::driver('facebook')->user();
+//             $user = Socialite::driver('facebook')->user();
          
-            $finduser = User::where('email', $user->email)->first();
+//             $finduser = User::where('email', $user->email)->first();
         
-            if($finduser){
+//             if($finduser){
          
-                Auth::login($finduser);
+//                 Auth::login($finduser);
         
-                return redirect()->intended('/');
+//                 return redirect()->intended('/');
          
-            }else{
-                $newUser = User::create([
-                    'name' => $user->name,
-                    'email' => $user->email,
-                    'facebook_id'=> $user->id,
-                    'password' => encrypt('123456789')
-                ]);
+//             }else{
+//                 $newUser = User::create([
+//                     'name' => $user->name,
+//                     'email' => $user->email,
+//                     'facebook_id'=> $user->id,
+//                     'password' => encrypt('123456789')
+//                 ]);
         
-                Auth::login($newUser);
+//                 Auth::login($newUser);
         
-                return redirect()->intended('/');
-            }
+//                 return redirect()->intended('/');
+//             }
         
-        } catch (Exception $e) {
-            dd($e->getMessage());
-        }
-    }
-}
+//         } catch (Exception $e) {
+//             dd($e->getMessage());
+//         }
+//     }
+// }
--- config/services.php
+++ config/services.php
@@ -1,44 +1,44 @@
 <?php
 
-return [
+// return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Third Party Services
-    |--------------------------------------------------------------------------
-    |
-    | This file is for storing the credentials for third party services such
-    | as Mailgun, Postmark, AWS and more. This file provides the de facto
-    | location for this type of information, allowing packages to have
-    | a conventional file to locate the various service credentials.
-    |
-    */
+//     /*
+//     |--------------------------------------------------------------------------
+//     | Third Party Services
+//     |--------------------------------------------------------------------------
+//     |
+//     | This file is for storing the credentials for third party services such
+//     | as Mailgun, Postmark, AWS and more. This file provides the de facto
+//     | location for this type of information, allowing packages to have
+//     | a conventional file to locate the various service credentials.
+//     |
+//     */
 
-    'mailgun' => [
-        'domain' => env('MAILGUN_DOMAIN'),
-        'secret' => env('MAILGUN_SECRET'),
-        'endpoint' => env('MAILGUN_ENDPOINT', 'api.mailgun.net'),
-    ],
+//     'mailgun' => [
+//         'domain' => env('MAILGUN_DOMAIN'),
+//         'secret' => env('MAILGUN_SECRET'),
+//         'endpoint' => env('MAILGUN_ENDPOINT', 'api.mailgun.net'),
+//     ],
 
-    'postmark' => [
-        'token' => env('POSTMARK_TOKEN'),
-    ],
+//     'postmark' => [
+//         'token' => env('POSTMARK_TOKEN'),
+//     ],
 
-    'ses' => [
-        'key' => env('AWS_ACCESS_KEY_ID'),
-        'secret' => env('AWS_SECRET_ACCESS_KEY'),
-        'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
-    ],
+//     'ses' => [
+//         'key' => env('AWS_ACCESS_KEY_ID'),
+//         'secret' => env('AWS_SECRET_ACCESS_KEY'),
+//         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
+//     ],
 
-    'google' => [
-        'client_id' => '636109156997-839b365mq8mk20pm8f03gl8bv52b7ikp.apps.googleusercontent.com',
-        'client_secret' => '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',
-        'redirect' => env('APP_URL').'/auth/google/callback',
-    ],
+//     'google' => [
+//         'client_id' => '636109156997-839b365mq8mk20pm8f03gl8bv52b7ikp.apps.googleusercontent.com',
+//         'client_secret' => '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',
+//         'redirect' => env('APP_URL').'/auth/google/callback',
+//     ],
 
-    'facebook' => [
-        'client_id' => 'app id',
-        'client_secret' => 'add secret',
-        'redirect' => env('APP_URL').'/auth/facebook/callback',
-    ],
-];
+//     'facebook' => [
+//         'client_id' => 'app id',
+//         'client_secret' => 'add secret',
+//         'redirect' => env('APP_URL').'/auth/facebook/callback',
+//     ],
+// ];
